e355fec6ce8e84d2c5031b67872d2835d02d7691
rc <eric.c.dand@gmail.com>
2017-12-10 15:41:59 -0800

    add go 1.9 to the linux zshrc

--- .zsh/linux_rc.zsh
+++ .zsh/linux_rc.zsh
@@ -7,3 +7,10 @@ alias pbpaste='xsel --clipboard --output'
 alias pbcopy='xsel --clipboard --input'
 
 alias ls="ls --color=always --group-directories-first -lG"
+
+# Source the latest version of go
+# You can install it like so:
+# 	sudo add-apt-repository ppa:gophers/archive
+# 	sudo apt update
+# 	sudo apt-get install golang-1.9.go
+export PATH="${PATH}:/usr/lib/go-1.9/bin"
